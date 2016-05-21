eb54afeb94efe7f49b78d470634e52298e276588
Chris Umbel <chris@chrisumbel.com>
2016-05-21 02:00:09 +0000

    initial import

---
+++ test/FirstMicroservice.UnitTests/TestTests.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace FirstMicroservice.UnitTests
+{
+    public class TestTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+            Assert.Equal(4, 4);
+        }
+    }
+}
