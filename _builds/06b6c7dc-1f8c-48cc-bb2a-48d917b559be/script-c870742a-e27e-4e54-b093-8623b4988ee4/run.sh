bb46844eb0a1116c43288530b6b1a5c19d4b2e5c
Chris Umbel <chris@chrisumbel.com>
2016-05-21 15:51:00 +0000

    test debugging

--- _builds/06b6c7dc-1f8c-48cc-bb2a-48d917b559be/script-c870742a-e27e-4e54-b093-8623b4988ee4/run.sh
+++
@@ -1,8 +0,0 @@
-set -e
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.1/install.sh | bash
-source $HOME/.nvm/nvm.sh
-nvm install 4
-nvm use 4
-npm install -g bower
-npm install -g gulp          
-echo '{ "allow_root": true }' > $HOME/.bowerrc
