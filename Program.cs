0e71a61f83a4ef7bd163e4c2548193d377551f5f
Chris Umbel <chris@chrisumbel.com>
2016-06-15 21:46:09 +0000

    hello-world start

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+
+namespace HelloWorld
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddCommandLine(args)
+            	.Build();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseConfiguration(config)
+                .Build();
+
+            host.Run();            
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+
+namespace HelloWorld {
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+        }        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello, world!");
+            });
+        }
+    }   
+}
