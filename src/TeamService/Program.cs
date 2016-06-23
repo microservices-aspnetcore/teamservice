845fdc1d57c114d3c80ae8e098bcedb727b33fcf
Kevin Hoffman <kehoffman@pivotal.io>
2016-06-23 14:11:25 -0400

    More factor of the re

--- src/TeamService/Program.cs
+++ src/TeamService/Program.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 
-namespace StatlerWaldorfCorp.FirstMicroservice
+namespace StatlerWaldorfCorp.TeamService
 {
     public class Program
     {
@@ -15,7 +15,7 @@ namespace StatlerWaldorfCorp.FirstMicroservice
 
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseStartup<StatlerWaldorfCorp.FirstMicroservice.Startup>()
+                .UseStartup<Startup>()
                 .UseConfiguration(config)
                 .Build();
 
