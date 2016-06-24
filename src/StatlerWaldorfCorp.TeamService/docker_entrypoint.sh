0548da4c99d71cd8d8bea50c385c6461aabc8ecb
Chris Umbel <chris@chrisumbel.com>
2016-06-24 20:20:12 +0000

    corrected assembly name

--- src/StatlerWaldorfCorp.TeamService/docker_entrypoint.sh
+++ src/StatlerWaldorfCorp.TeamService/docker_entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /pipeline/source/app/publish
-dotnet FirstMicroservice.dll --server.urls=http://0.0.0.0:${PORT-"8080"}
+dotnet StatlerWaldorfCorp.TeamService.dll --server.urls=http://0.0.0.0:${PORT-"8080"}
