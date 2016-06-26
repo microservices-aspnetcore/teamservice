55530a6dff218b520e6f2590bd52dab98cd1997f
Chris Umbel <chris@chrisumbel.com>
2016-06-26 23:17:50 +0000

    added first and last name to Member

--- src/StatlerWaldorfCorp.TeamService/Models/Member.cs
+++ src/StatlerWaldorfCorp.TeamService/Models/Member.cs
@@ -4,9 +4,16 @@ namespace StatlerWaldorfCorp.TeamService.Models
 {
     public class Member {
         public Guid ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
         public Member(Guid id) {
             this.ID = id;
         }
+
+        public Member(string firstName, string lastName, Guid id) : this(id) {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }        
     }
 }
