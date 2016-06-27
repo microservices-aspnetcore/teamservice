9d6bd52ac9c19af50131fcfd2461d4687fb81cf1
Chris Umbel <chris@chrisumbel.com>
2016-06-27 03:04:17 +0000

    added Member PUT

--- test/StatlerWaldorfCorp.TeamService.Tests/MembersControllerTest.cs
+++ test/StatlerWaldorfCorp.TeamService.Tests/MembersControllerTest.cs
@@ -41,5 +41,55 @@ namespace StatlerWaldorfCorp.TeamService
 
             Assert.True(result is NotFoundResult);
         }
+
+        [Fact]
+        public async void UpdateMemberOverwrites() 
+        {
+            ITeamRepository repository = new TestMemoryTeamRepository();
+            MembersController controller = new MembersController(repository);
+
+            Guid teamId = Guid.NewGuid();
+            Team team = new Team("TestController", teamId);
+            repository.AddTeam(team);        
+
+            Guid memberId = Guid.NewGuid();
+            Member newMember = new Member(memberId);
+            newMember.FirstName = "Jim";
+            newMember.LastName = "Smith";
+            await controller.CreateMember(newMember, teamId);
+
+            Member updatedMember = new Member(memberId);
+            updatedMember.FirstName = "Bob";
+            updatedMember.LastName = "Jones";            
+            await controller.UpdateMember(updatedMember, teamId, memberId);            
+
+            team = repository.GetTeam(teamId);
+            Member testMember = team.Members.Where(m => m.ID == memberId).First();
+            Assert.Equal(testMember.FirstName, "Bob");
+            Assert.Equal(testMember.LastName, "Jones");            
+        }           
+
+        [Fact]
+        public async void UpdateMembertoNonexistantMemberReturnsNoMatch() 
+        {
+            ITeamRepository repository = new TestMemoryTeamRepository();
+            MembersController controller = new MembersController(repository);
+
+            Guid teamId = Guid.NewGuid();
+            Team team = new Team("TestController", teamId);
+            repository.AddTeam(team);        
+
+            Guid memberId = Guid.NewGuid();
+            Member newMember = new Member(memberId);
+            newMember.FirstName = "Jim";
+            await controller.CreateMember(newMember, teamId);
+
+            Guid nonMatchedGuid = Guid.NewGuid();
+            Member updatedMember = new Member(nonMatchedGuid);
+            updatedMember.FirstName = "Bob";
+            var result = await controller.UpdateMember(updatedMember, teamId, nonMatchedGuid);            
+
+            Assert.True(result is NotFoundResult);
+        }                   
     }
 }
