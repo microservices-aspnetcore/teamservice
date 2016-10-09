93bc10a75f2018694a8660a9e5bb7515da835c75
Chris Umbel <chris@chrisumbel.com>
2016-10-09 18:04:40 -0400

    started integration testing

---
+++ test/StatlerWaldorfCorp.TeamService.Integration/TeamsControllerTest.cs
@@ -0,0 +1,17 @@
+using Xunit;
+using System.Collections.Generic;
+using StatlerWaldorfCorp.TeamService.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace StatlerWaldorfCorp.TeamService.Integration
+{
+    public class PostgresTest
+    {	    
+        [Fact]
+        public void Integration()
+        {             
+        }
+    }
+}
