45a12af48898e28aed6e6e23f781b6c0bd1ddc18
Chris Umbel <chris@chrisumbel.com>
2016-10-09 20:42:51 -0400

    Insert Postgres test

--- test/StatlerWaldorfCorp.TeamService.Integration/PostgresIntegrationTest.cs
+++ test/StatlerWaldorfCorp.TeamService.Integration/PostgresIntegrationTest.cs
@@ -4,14 +4,45 @@ using StatlerWaldorfCorp.TeamService.Models;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Linq;
+using Steeltoe.Extensions.Configuration;
+using Steeltoe.CloudFoundry.Connector.PostgreSql.EFCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using StatlerWaldorfCorp.TeamService.Persistence;
+using StatlerWaldorfCorp.TeamService.Models;
 
 namespace StatlerWaldorfCorp.TeamService.Integration
 {
     public class PostgresIntegrationTest
     {	    
+        private IConfigurationRoot config;
+        private ApplicationDbContext context;
+
+        public PostgresIntegrationTest()
+        {
+			config = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddCloudFoundry()
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            optionsBuilder.UseNpgsql(config);
+            this.context = new ApplicationDbContext(optionsBuilder.Options);                
+        }
+
         [Fact]
-        public void Integration()
-        {             
+        public void ShouldInsert()
+        {
+            Guid id = Guid.NewGuid();
+            Team team = new Team() {Name = "Team " + id.ToString(), ID = id};
+
+            PostgresTeamRepository repository = new PostgresTeamRepository(context);
+            repository.AddTeam(team);
+
+            Team savedTeam = repository.GetTeam(id);
+
+            Assert.NotNull(savedTeam);
+            Assert.Equal(savedTeam.ID, id);
         }
     }
 }
