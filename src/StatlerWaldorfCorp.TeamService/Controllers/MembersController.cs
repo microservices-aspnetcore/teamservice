141860b750887f53cf705e3f4502b777b1347d30
Kevin Hoffman <kehoffman@pivotal.io>
2016-12-31 13:19:57 -0500

    Refactoring namespace of ITeamRepository to Persistence (was models)

--- src/StatlerWaldorfCorp.TeamService/Controllers/MembersController.cs
+++ src/StatlerWaldorfCorp.TeamService/Controllers/MembersController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using StatlerWaldorfCorp.TeamService.Models;
 using System.Threading.Tasks;
+using StatlerWaldorfCorp.TeamService.Persistence;
 
 namespace StatlerWaldorfCorp.TeamService
 {
